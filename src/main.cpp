94213c2ecf1d1bb6805e815abb0568c10286da91
JR <jrchang11015043@gmail.com>
2025-03-31 01:25:05 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "./include/armadillo"
+#include "EasyBMP.h"
+#include "EasyBMP_Geometry.h"
+#include "Gordian.h"
+
+using namespace arma;
+using namespace std;
+
+int main(void) {
+    Gordian Gord;
+
+    Gord.processFunc();
+    while (1);
+    return 1;
+}
